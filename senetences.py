983137918a59583b46a110f4665ea5e269621606
Brigham Aviles <avilesbrigham14@gmail.com>
2022-05-22 07:52:02 -0600

    dd

--- senetences.py
+++ senetences.py
@@ -10,11 +10,21 @@ def get_determiner(quantity):
           words = ["a", "one", "the"]
     else:
         words = ["two", "some", "many", "the"]
+
     word = random.choice(words)
     return word
 
-def get_noun():
+def get_noun(quantity):
+
+    words = [ "bird", "boy", "car", "cat", "child","dog", "girl", "man", "rabbit", "woman", "birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    if quantity == 1:
+        words = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
+    else:
+        words = [ "birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
+
+    word = random.chice(words)
+    return word
 
 
-def get_verb():
 
