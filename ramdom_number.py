d796e96da1b2520fa22a6c89a0af6b90aab752fc
Brigham Aviles <avilesbrigham14@gmail.com>
2022-06-01 19:42:14 -0600

    ss

---
+++ ramdom_number.py
@@ -0,0 +1,33 @@
+import random
+
+
+
+def main():
+
+    numbers = [16.2, 75.1, 52.3]
+
+
+    print(numbers)
+
+    append_random_numbers(numbers)
+
+    print(number)
+    append_random_numbers(number, 3)
+
+    print(numbers)
+
+
+
+
+
+
+def append_random_numbers(numbers_list, quantity = 1):
+
+
+    for _ in range (quantity):
+        ran_number = random.uniform(0, 100)
+        round_number = round(ran_number, 1)
+        numbers_list.append (round_number)
+
+
+main()
